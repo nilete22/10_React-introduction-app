09d218805fa2a9f35596a78bdd1c64d4ef7e156e
Nil Farras <farrasnil@gmail.com>
2024-01-31 17:15:03 +0100

    part 2 of the exercice done!

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import { Header} from "./Header.jsx";
+import { Total } from "./Total.jsx";
+import { Content } from "./Content.jsx";
 
-function App() {
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
-  const name = "Nil Farras"
   return (
-    <>
-      <h1>{`Hello ${name}`}</h1>
-    </>
+    <div>
+      <Header course={ course }/>
+      <Content section1={part1} ex1={exercises1} section2={part2} ex2={exercises2} section3={part3} ex3={exercises3} />
+      <Total ex1={exercises1} ex2={exercises2} ex3={exercises3} />
+    </div>
   )
 }
 
---
+++ src/Header.jsx
@@ -0,0 +1,5 @@
+export const Header = ({ course }) => {
+    return (
+        <h1>{course}</h1>
+    );
+}
